c3936fb53064294fc4394091e0d19737752db887
Joshua Adams <joshadams554@gmail.com>
2018-10-08 15:22:03 -0400

    server working

--- server.js
+++ server.js
@@ -9,12 +9,21 @@ console.log('Hello Noteful!');
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(data);
+});
+
+
 app.listen(8080, function () {
     console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
     console.error(err);
 });
 
-app.get('/api/notes', (req, res) => {
-    res.json(data);
-});
+
+
+// app.get('/api/notes/:id', (req, res) => {
+//     res.json(data.id);
+// });
