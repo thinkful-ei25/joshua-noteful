e657ff37469a53f39235e260c82cfc6c430a158a
Joshua Adams <joshadams554@gmail.com>
2018-10-08 14:41:26 -0400

    getting up express

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const app = express();
+
+app.listen(8080, function () {
+    console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+    console.error(err);
+});
